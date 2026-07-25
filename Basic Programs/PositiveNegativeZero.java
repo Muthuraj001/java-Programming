f1769fae271b6ccb8f477067fadcd47f1d9f7140
muthuraj.p001 <muthuraj.p001@gmail.com>
2026-07-25 14:46:44 +0530

    Add EvenorOdd and PositiveNegativeZero classes to determine number properties

---
+++ Basic Programs/PositiveNegativeZero.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PositiveNegativeZero {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        double number = sc.nextDouble();
+
+        if (number > 0) {
+            System.out.println(number + " is a positive number.");
+        } else if (number < 0) {
+            System.out.println(number + " is a negative number.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+    }
+    
+}

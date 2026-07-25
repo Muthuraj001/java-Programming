e7b8987702d1e72b3b516684bd5451834cabcda9
muthuraj.p001 <muthuraj.p001@gmail.com>
2026-07-25 15:05:42 +0530

    Fix comparison logic in LargestofThreeNumbers class to handle equal values correctly

--- Basic Programs/LargestofThreeNumbers.java
+++ Basic Programs/LargestofThreeNumbers.java
@@ -11,12 +11,14 @@ public class LargestofThreeNumbers {
         System.out.print("Enter third number : ");
         int num3 = sc.nextInt();
 
-        if (num1 >= num2 && num1 >= num3) {
+        if (num1 > num2 && num1 > num3) {
             System.out.println(num1 + " is the largest number.");
-        } else if (num2 >= num1 && num2 >= num3) {
+        } else if (num2 > num1 && num2 > num3) {
             System.out.println(num2 + " is the largest number.");
-        } else {
+        } else if (num3 > num1 && num3 > num2) {
             System.out.println(num3 + " is the largest number.");
+        } else {
+            System.out.println("All numbers are equal.");
         }
     }
 }
